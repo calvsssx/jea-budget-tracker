48a2e6edf0a988840805b50898e00856e4ce62f3
Calvin <calvin.quiambao@gmail.com>
2026-05-17 16:49:02 +0800

    removed env

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvSgVznuvkuw69fCftj7ZYkTd6MzTw0Z4",
-  authDomain: "jea-budget-tracker.firebaseapp.com",
-  projectId: "jea-budget-tracker",
-  storageBucket: "jea-budget-tracker.firebasestorage.app",
-  messagingSenderId: "384416806544",
-  appId: "1:384416806544:web:ab19354f4050d49ff74a15",
-  measurementId: "G-3SLR5602NB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
